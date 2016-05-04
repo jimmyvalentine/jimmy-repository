21f397237d38b2285764e63545e731e8e05a6a45
Sajeev Rajput <sajeev.rajput@gmail.com>
2016-05-05 03:58:23 +0530

    commiting my first java project to Git.

---
+++ SortingAlgorithms/src/SortingAlgo.java
@@ -0,0 +1,41 @@
+
+public class SortingAlgo {
+
+	public static void main(String[] args) {
+		
+		int arr[]={6,3,5,1,8,2};
+		
+		System.out.println("Pre-sorting...");
+		display(arr);
+		
+		System.out.println("\n\nAfter sorting...");
+		insertionSort(arr);
+		display(arr);
+	
+		
+	}
+	//end of main()
+	
+	//sorting function
+	public static int[] insertionSort(int arr[]){
+		int i,j,key;
+		for(j=1; j<arr.length; j++){
+			key=arr[j];
+			i=j-1;
+			while(i>=0 && arr[i]>key){
+				arr[i+1]=arr[i];
+				i=i-1;
+			}
+			arr[i+1]=key;
+		}
+		return arr;
+	}
+	
+	//display the sorted array
+	public static void display(int arr[]){
+		for(int i=0;i<arr.length;i++)
+			System.out.print(arr[i]+" ");
+
+	}
+
+}
